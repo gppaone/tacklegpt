f4dde8ace1974043d177b64a4a37237146a95995
gppaone_ncstate <gppaone@ncsu.edu>
2026-01-14 08:24:46 -0500

    head info

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,5 +5,12 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+    <link rel="icon" href={favicon} />
+    <title>TackleGPT - Your Fishing Advisor</title>
+    <meta name="description" content="Get a tactical audible when the bite is slow. Ask me anything about fishing!">
+    <meta name="keywords" content="fishing, advice, tackle, gear, weather, tactics">
+    <meta name="author" content="TackleGPT">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</svelte:head>
 {@render children()}
